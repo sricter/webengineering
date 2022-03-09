11fa33b360490bd6f6b7e89eb0a43827b81aff08
leonschult <schulthl@dhbw-loerrach.de>
2022-03-09 18:42:27 +0100

    server

--- index.js
+++ index.js
@@ -6,22 +6,14 @@ const socketio = require('socket.io');
 // Server
 const app = express();
 const server = http.createServer(app);
-
-const PORT = 8081 || process.env.PORT;
-server.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`)); //run Server
-
 const io = socketio(server); //Socketio für Server verwenden
 
 //Ordner als Standard setzen -> mit __dirname aktuelles Verzeichnis
 app.use(express.static(path.join(__dirname, 'frontend'))); 
 
-io.on('connection', (socket) =>{
-    socket.emit('message','Willkommen im Chat');
-
-    socket.broadcast.emit('message','Neuer User im Chat');
-
-    socket.on('disconnect', () => {
-        io.emit('message', 'User hat Chat verlassen');
-    });
+io.on('connection', socket =>{
+    console.log('neue Connection');
 });
 
+const PORT = 3000 || process.env.PORT;
+server.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`)); //run Server
